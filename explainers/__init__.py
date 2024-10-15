e33f0ef48e3137b26adb692df9ce880dc747865e
themaigod <2271108464@qq.com>
2024-10-15 21:23:28 +0800

    config setting for new explainer

--- explainers/__init__.py
+++ explainers/__init__.py
@@ -9,6 +9,7 @@ __all__ = [
     # "GOAtExplainer",
     # "SubgraphXExplainer",
     "GNNExplainerMeta",
+    "GNNExplainerOriginal",
     # "PGExplainer",
     # "CGEExplainer",
     "Explainer",
@@ -18,7 +19,7 @@ from .grad import GradExplainerMeta, GradExplainer, GradExplainerOriginal
 # from .saliency import SAExplainer
 # from .goat import GOAtExplainer
 # from .subgraphx import SubgraphXExplainer
-from .gnnexplainer import GNNExplainerMeta
+from .gnnexplainer import GNNExplainerMeta, GNNExplainerOriginal
 # from .pgexplainer import PGExplainer
 # from .cge import CGEExplainer
 from .explainer import Explainer
@@ -48,6 +49,8 @@ def load_explainer(explainer_name, model_name, dataset_name, explainer_config=No
 
     if explainer_name == "GNNExplainerMeta":
         return GNNExplainerMeta(config)
+    elif explainer_name == "GNNExplainerOriginal":
+        return GNNExplainerOriginal(config)
     elif explainer_name == "GradExplainerMeta":
         return GradExplainerMeta(config)
     elif explainer_name == "GradExplainer":
