0689644893d27cbac7b383f8561810564379cc2d
themaigod <2271108464@qq.com>
2024-10-23 01:22:34 +0800

    update grad

--- main_mutiple_times.py
+++ main_mutiple_times.py
@@ -324,7 +324,9 @@ def prepare_explainer_configs(explainer_configs, times, **kwargs):
                 new_config[key] = value
         with open(new_config_path, "w") as f:
             json.dump(new_config, f)
-        time.sleep(0.5)
+            f.flush()
+            os.fsync(f.fileno())
+        time.sleep(0.1)
         yield new_config_path
 
 
